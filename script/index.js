c0e6580143f36e9c0b30577bdfaa9f37889669da
Wanderson Matheus <126834974+wmatheus190900@users.noreply.github.com>
2024-11-12 13:09:21 -0300

    mudando o header

--- script/index.js
+++ script/index.js
@@ -7,53 +7,33 @@ checkbox.addEventListener("change", ()=>{
         html.setAttribute("data-bs-theme" , "light");
     }
 })
-document.addEventListener("scroll", () => {
-    const sections = document.querySelectorAll("section");
-    const navLinks = document.querySelectorAll(".nav-link");
-  
-    sections.forEach((section, index) => {
-      const rect = section.getBoundingClientRect();
-      if (rect.top <= 100 && rect.bottom >= 100) {
-        navLinks.forEach((link) => link.classList.remove("active"));
-        navLinks[index].classList.add("active");
-      }
-    });
-});
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    const navbarLinks = document.querySelectorAll(".nav-link");
-  
-    function removeActiveClass() {
-      navbarLinks.forEach(link => {
-        if (link) {
-          link.classList.remove("active");
-        }
-      });
-    }
-  
-    function highlightSection() {
-      const sections = document.querySelectorAll("section");
-      let currentSection = "";
-  
-      sections.forEach(section => {
-        const sectionTop = section.offsetTop - 70;
-        if (window.scrollY >= sectionTop) {
-          currentSection = section.getAttribute("id");
-        }
-      });
-  
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".nav-link");
 
-      removeActiveClass();
-      navbarLinks.forEach(link => {
-        if (link && currentSection && link.getAttribute("href") === `#${currentSection}`) {
-          link.classList.add("active");
-        }
-      });
-    }
-  
-    window.addEventListener("scroll", highlightSection);
-    highlightSection();
-  });
+  function highlightSection() {
+    let currentSectionId = "";
+
+
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop - 70;
+      const sectionBottom = sectionTop + section.offsetHeight;
+      
+      if (window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
+        currentSectionId = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach(link => {
+      link.classList.toggle("active", link.getAttribute("href") === `#${currentSectionId}`);
+    });
+  }
+
+  window.addEventListener("scroll", highlightSection);
+  highlightSection(); 
+});
+
   
   
